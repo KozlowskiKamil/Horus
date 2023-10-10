d8c3c982269f98e54be254e737152385b95cf08f
KozlowskiKamil <kamil2k8@gmail.com>
2023-10-11 00:33:47 +0200

    add readme.md -task description

--- src/main/java/pl/horus/Wall.java
+++ src/main/java/pl/horus/Wall.java
@@ -1,24 +1,39 @@
 package pl.horus;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class Wall implements Structure{
-
+public class Wall implements Structure {
     private List<Block> blocks;
+
+    public Wall(List<Block> blocks) {
+        this.blocks = blocks;
+    }
 
     @Override
     public Optional<Block> findBlockByColor(String color) {
+        for (Block block : blocks) {
+            if (block.getColor().equals(color)) {
+                return Optional.of(block);
+            }
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Block> findBlocksByMaterial(String material) {
-        return null;
+        List<Block> matchingBlocks = new ArrayList<>();
+        for (Block block : blocks) {
+            if (block.getMaterial().equals(material)) {
+                matchingBlocks.add(block);
+            }
+        }
+        return matchingBlocks;
     }
 
     @Override
     public int count() {
-        return 0;
+        return blocks.size();
     }
 }
